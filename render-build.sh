3c8c3643dac8f2fd1417baa166879f7c187ecd63
tadp6le <ravatiyarahul@gmail.com>
2026-06-24 16:56:21 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+npm install
+
+# Ensure the Puppeteer cache directory exists
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+mkdir -p $PUPPETEER_CACHE_DIR
+
+# Install Puppeteer and download Chrome
+npx puppeteer browsers install chrome
+
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+  echo "...Copying Puppeteer Cache from Build Cache"
+  cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+else
+  echo "...Storing Puppeteer Cache in Build Cache"
+  cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+fi
